c37d9ce1927d3c161738ed9fe7f65da6577ea7b9
GabrielmAlves <gabrielmalves10@gmail.com>
2024-03-22 17:46:16 -0300

    more ViewModel files

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerClassifier.WPF.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //definindo properties que vão estabelecer o binding entre View e ViewModel
+
+        private string _username;
+        private string _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true; //para ver se a View é visível (se o login der certo, hide the view)
+    }
+}
--- ViewModel/ViewModelBase.cs
+++ ViewModel/ViewModelBase.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace PlayerClassifier.WPF.ViewModel
 {
-    class ViewModelBase
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged; //avisa binding clients que uma property mudou e os valores precisam ser reavaliados
+
+        public void OnPropertyChanged (string propertyName) //cria o evento quando uma property mudou
+        {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+        }
     }
 }
