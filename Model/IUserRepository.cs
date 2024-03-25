a8a2e9803fcf7b6a4caedf77de177cba2d794e2d
GabrielmAlves <gabrielmalves10@gmail.com>
2024-03-24 22:56:44 -0300

    [TCC24-12] implementing the repository base class: referencing sql client

--- Model/IUserRepository.cs
+++ Model/IUserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,12 @@ namespace PlayerClassifier.WPF.Model
 {
     public interface IUserRepository
     {
+        bool AuthenticateUser (NetworkCredential credential);
+        void Add(UserModel userModel);
+        void Edit (UserModel userModel);
+        void Remove(int id);
+        UserModel GetById (int id);
+        UserModel GetByEmail (string email);
+        IEnumerable<UserModel> GetByAll();
     }
 }
--- obj/Debug/net8.0-windows/PlayerClassifier.WPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PlayerClassifier.WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayerClassifier.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884fbb6f6653454082f74d0a40826f0b4bc6633d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7c3d576b315a62065be4a48abcd0d50dab2570")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayerClassifier.WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayerClassifier.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
