56158db73409eeb084cb276039699d85f811dba4
GabrielmAlves <gabrielmalves10@gmail.com>
2024-03-28 19:33:30 -0300

    [TCC24-3 some more adjustments]

--- View/SignUpView.xaml.cs
+++ View/SignUpView.xaml.cs
@@ -23,5 +23,23 @@ namespace PlayerClassifier.WPF.View
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
