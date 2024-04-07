668f0ea8dda289be5376db6149147fd0cf905e26
GabrielmAlves <gabrielmalves10@gmail.com>
2024-04-07 20:20:03 -0300

    [TCC24-6] creating buttons and styles

--- obj/Debug/net8.0-windows/View/SignUpView.g.i.cs
+++ obj/Debug/net8.0-windows/View/SignUpView.g.i.cs
@@ -21,6 +21,7 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.Integration;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Markup;
@@ -111,7 +112,7 @@ namespace PlayerClassifier.WPF.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PlayerClassifier.WPF;component/view/signupview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PlayerClassifier.WPF;V1.0.0.0;component/view/signupview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\SignUpView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
