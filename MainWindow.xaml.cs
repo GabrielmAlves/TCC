9cdebb075862a7d3625dba66323bcaecbfed1f42
GabrielmAlves <gabrielmalves10@gmail.com>
2024-04-07 21:10:51 -0300

    [TCC24-6] implementing drag move and other things

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Windows.Interop;
 
 namespace PlayerClassifier.WPF
 {
@@ -20,5 +23,22 @@ namespace PlayerClassifier.WPF
             {
                 InitializeComponent();
             }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
+        //private void PnlControlBar_MouseEnter(object sender, MouseButtonEventArgs e)
+        //{
+        //    this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        //}
+    }
 }
