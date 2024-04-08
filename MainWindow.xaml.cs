c1b89a2b3f70bb231bf408e55b99505031f2063b
GabrielmAlves <gabrielmalves10@gmail.com>
2024-04-08 01:16:00 -0300

    [TCC24-6] adjusting home view

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,9 +36,27 @@ namespace PlayerClassifier.WPF
                 DragMove();
             }
         }
-        //private void PnlControlBar_MouseEnter(object sender, MouseButtonEventArgs e)
-        //{
-        //    this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-        //}
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click (object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else 
+                this.WindowState = WindowState.Normal;
+        }
     }
 }
