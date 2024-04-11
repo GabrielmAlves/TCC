6d9fdc0379fc0c57850f752bdf117b5695291827
GabrielmAlves <gabrielmalves10@gmail.com>
2024-04-11 00:18:58 -0300

    [TCC24-25] navigation done

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using FontAwesome.Sharp;
 using PlayerClassifier.WPF.Model;
 using PlayerClassifier.WPF.Repositories;
 
@@ -12,16 +14,109 @@ namespace PlayerClassifier.WPF.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private ViewModelBase _currentChildView;
+        private string _childViewName;
+        private IconChar _icon;
         private UserAccountModel __currentUserAccount;
         private IUserRepository userRepository;
         
         public UserAccountModel CurrentUserAccount { get { return __currentUserAccount; } set { __currentUserAccount = value; OnPropertyChanged(nameof(CurrentUserAccount)); } }
 
+        public ViewModelBase CurrentChildView 
+        { 
+            get
+            { 
+                return _currentChildView; 
+            } 
+            set 
+            { 
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            } 
+        }
+        public string ChildViewName
+        {
+            get
+            {
+                return _childViewName;
+            }
+            set
+            {
+                _childViewName = value;
+                OnPropertyChanged(nameof(ChildViewName));
+            }
+        }
+        public IconChar Icon
+        {
+            get
+            {
+                return _icon;
+            }
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowClassifyViewCommand { get; }
+        public ICommand ShowCompareViewCommand { get; }
+        public ICommand ShowHistoryViewCommand { get; }
+        public ICommand ShowObservationViewCommand { get; }
+
+
+
         public MainViewModel()
         {
             userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowClassifyViewCommand = new ViewModelCommand(ExecuteShowClassifyViewCommand);
+            ShowCompareViewCommand = new ViewModelCommand(ExecuteShowCompareViewCommand);
+            ShowHistoryViewCommand = new ViewModelCommand(ExecuteShowHistoryViewCommand);
+            ShowObservationViewCommand = new ViewModelCommand(ExecuteShowObservationViewCommand);
+
+            ExecuteShowHomeViewCommand(null);
+
             loadCurrentUserData();
+        }
+
+        private void ExecuteShowObservationViewCommand(object obj)
+        {
+            CurrentChildView = new ObservationViewModel();
+            ChildViewName = "Em observação";
+            Icon = IconChar.Binoculars;
+        }
+
+        private void ExecuteShowHistoryViewCommand(object obj)
+        {
+            CurrentChildView = new HistoryViewModel();
+            ChildViewName = "Histórico";
+            Icon = IconChar.History;
+        }
+
+        private void ExecuteShowCompareViewCommand(object obj)
+        {
+            CurrentChildView = new CompareViewModel();
+            ChildViewName = "Compare";
+            Icon = IconChar.FutbolBall;
+        }
+
+        private void ExecuteShowClassifyViewCommand(object obj)
+        {
+            CurrentChildView = new ClassifyPlayerViewModel();
+            ChildViewName = "Classificar jogador";
+            Icon = IconChar.FutbolBall;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            ChildViewName = "Home";
+            Icon = IconChar.Home;
         }
 
         private void loadCurrentUserData()
