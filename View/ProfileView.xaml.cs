7f3c7fbe48f46760ce721dc8be601a524ad76d41
GabrielmAlves <gabrielmalves10@gmail.com>
2024-04-14 15:58:52 -0300

    [TCC24-32] adding user's picture

--- View/ProfileView.xaml.cs
+++ View/ProfileView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+//using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +26,23 @@ namespace PlayerClassifier.WPF.View
         public ProfileView()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectPicture_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivos csv | *.png;*.jpg;*.jpeg";
+            bool? fileOpened = openFileDialog.ShowDialog();
+
+            if (fileOpened == true)
+            {
+                string path = openFileDialog.FileName;
+                string fileName = openFileDialog.SafeFileName;
+                imageName.ImageSource = new BitmapImage(new Uri(openFileDialog.FileName));
+            } else
+            {
+
+            }
         }
     }
 }
