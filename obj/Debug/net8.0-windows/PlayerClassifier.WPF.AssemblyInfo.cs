3e583d60c68cd5076c3043cd7290f7b18c21ec54
GabrielmAlves <gabrielmalves10@gmail.com>
2024-06-02 22:48:15 -0300

    adding things

--- obj/Debug/net8.0-windows/PlayerClassifier.WPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PlayerClassifier.WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayerClassifier.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87320dfc30638b0ffe76a07725b3fc21babfb35c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e50bef8a8ff0412c08b8cbface804037c8208c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayerClassifier.WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayerClassifier.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
